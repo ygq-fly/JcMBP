753f69c097bf11668d56eff5766ffb374f102f24
ygq-fly <867069250@qq.com>
2016-12-05 09:38:45 +0800

    2016.12.5（86）
    1.点频测试从根据点数修改为时间
    2.修改了tx1补偿无效，

--- JcMBP/PoiTimeSweepLeft.cs
+++ JcMBP/PoiTimeSweepLeft.cs
@@ -70,12 +70,12 @@ namespace JcMBP
             ds.time1 = (double)time;
             OfftenMethod.ToAddColumns(ds.dt);
             OfftenMethod.ToAddColumns(ds.dtc);
-            if (checkBox1.Checked)
-                ds.time_off1 = ds.off1 = Convert.ToDouble(numericUpDown3.Value);
+            if (time_check_off1.Checked)
+                ds.time_off1 = ds.off1 = Convert.ToDouble(time_nud_off1.Value);
             else
                 ds.off1 =  0;
-            if(time_check_off1.Checked)
-               ds.time_off2= ds.off2 = Convert.ToDouble(time_nud_off1.Value);
+            if(time_check_off2.Checked)
+               ds.time_off2= ds.off2 = Convert.ToDouble(time_nud_off2.Value);
             else
                 ds.off2 = 0;
 
